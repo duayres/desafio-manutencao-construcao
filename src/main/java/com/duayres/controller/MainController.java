b0c2214496ed258ea4c51024612d9bd2ae04c976
duayres <ayreseduardo@hotmail.com>
2016-11-01 08:51:19 -0200

    (upd) pom (+)teste de controller

--- src/main/java/com/duayres/controller/MainController.java
+++ src/main/java/com/duayres/controller/MainController.java
@@ -1,5 +1,13 @@
 package com.duayres.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class MainController {
 
+
+	@RequestMapping(value="/")
+	public void teste(){
+		System.out.println("testão! xD");
+	}	
+	
 }
